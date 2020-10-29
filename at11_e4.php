12b7855598b3585537009ebd6e7da0c623abae1d
Professor <Professor@WIN-MP3JADQD58A.local>
2020-10-29 12:11:24 +0000

    Update at11_e4.php

--- at11_e4.php
+++ at11_e4.php
@@ -2,9 +2,19 @@
 	$a=array('a'=>'macâ','b'=>'banana');
 	$b=array('d'=>'kiwi','e'=>'ananás','c'=>'morango');
 	$ab=array_merge($a,$b);
-	var_dump($ab);
+	echo "União de a + b";
+	echo '<br>';
+	foreach ($ab as $key => $bas) {
+		echo $key. '-' .$bas;
+		echo '<br>';
+	}
 	echo "<br>";
 	$ba=array_merge($b,$a);
-	var_dump($ba);
+	echo "União de b + a";
+	echo '<br>';
+	foreach ($ba as $key => $bas) {
+		echo $key. '-' .$bas;
+		echo '<br>';
+	}
 
 ?>
